9442166142b0827c9a88858fc40b7eae4ba5f588
Manish Thakur <manishtha177@gmail.com>
2023-02-10 11:12:41 +0530

    Refactor: Refactor code and added css for styling Character page

--- src/components/starwars/Character.tsx
+++ src/components/starwars/Character.tsx
@@ -1,40 +1,35 @@
-import { Link } from "react-router-dom";
+import { Card } from "react-bootstrap";
+import "./Character.css";
 
 const Character = (starwars: any) => {
   return (
     <div>
-      {starwars?.data?.length &&
-        starwars?.data.map((data: any, index: string) => (
-          <div key={index}>
-            <div>
-              <span>
-                <strong>Name: </strong>
-              </span>
-              <Link to={`/people/${data?.url.substr(-2, 1)}`}>
-                {data?.name}
-              </Link>
+      <div className="__heading">
+        <h1>Starwars Collections</h1>
+      </div>
+      <div className="row row-cols-3 g-3">
+        {starwars?.data?.length &&
+          starwars?.data.map((data: any, index: string) => (
+            <div key={index} className="__star-wars col">
+              <Card className="__character-cards shadow-lg bg-grey rounded">
+                <Card.Body>
+                  <Card.Title>
+                    <Card.Link href={`/people/${data?.url.substr(-2, 1)}`}>
+                      {data?.name}
+                    </Card.Link>
+                  </Card.Title>
+                  <Card.Subtitle>{data?.gender}</Card.Subtitle>
+                </Card.Body>
+                <Card.Body>
+                  <Card.Link href={`${data?.homeworld}`}>
+                    {data?.homeworld}
+                  </Card.Link>
+                  <Card.Link href={`${data?.url}`}>{data?.url}</Card.Link>
+                </Card.Body>
+              </Card>
             </div>
-            <div>
-              <span>
-                <strong>Gender: </strong>
-              </span>
-              {data?.gender}
-            </div>
-            <div>
-              <span>
-                <strong>Home Planet: </strong>
-              </span>
-              {data?.homeworld}
-            </div>
-            <div>
-              <span>
-                <strong>URL: </strong>
-              </span>
-              {data?.url}
-            </div>
-            <hr />
-          </div>
-        ))}
+          ))}
+      </div>
     </div>
   );
 };
