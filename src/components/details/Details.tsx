05ce8cec7f510f46ecf7ed6720077b33b8516205
Manish Thakur <manishtha177@gmail.com>
2023-02-10 11:14:36 +0530

    Refactor: Refactor code and added css for styling Details page

--- src/components/details/Details.tsx
+++ src/components/details/Details.tsx
@@ -1,9 +1,10 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import { useParams } from "react-router";
+import { Card } from "react-bootstrap";
 import { PeopleResponse } from "../../types/peopleType";
 import LoadingSpinner from "../loadingSpinner";
+import "./Details.css";
 
 const Details = () => {
   const { id } = useParams();
@@ -37,23 +38,30 @@ const Details = () => {
   return (
     <div>
       {!loading ? (
-        <div>
-          <strong>Name: </strong>
-          <div>{starData?.name}</div>
-          <strong>Hair Color: </strong>
-          <div>{starData?.hair_color}</div>
-          <strong>Eye Color: </strong>
-          <div>{starData?.eye_color}</div>
-          <strong>Gender: </strong>
-          <div>{starData?.gender}</div>
-          <strong>Home Planet: </strong>
-          <div>{starData?.homeworld}</div>
-          <strong>Films: </strong>
-          {starData?.films.map((film, index) => (
-            <div key={index}>
-              <Link to={`${film}`}>{film}</Link>
-            </div>
-          ))}
+        <div className="__starwars">
+          <Card className="__cards shadow-lg bg-grey rounded">
+            <Card.Title className="__character-title">
+              {starData?.name}
+            </Card.Title>
+            <Card.Body className="__cards-details">
+              <Card.Subtitle>Hair Color:</Card.Subtitle>
+              <Card.Text> {starData?.hair_color}</Card.Text>
+              <Card.Subtitle>Eye Color: </Card.Subtitle>
+              <Card.Text>{starData?.eye_color}</Card.Text>
+              <Card.Subtitle>Gender: </Card.Subtitle>
+              <Card.Text>{starData?.gender}</Card.Text>
+              <Card.Subtitle>Home PLanet:</Card.Subtitle>
+              <Card.Link href={`${starData?.homeworld}`}>
+                {starData?.homeworld}
+              </Card.Link>
+              <Card.Subtitle>Films</Card.Subtitle>
+              {starData?.films.map((film, index) => (
+                <div key={index}>
+                  <Card.Link href={`${film}`}>{film}</Card.Link>
+                </div>
+              ))}
+            </Card.Body>
+          </Card>
         </div>
       ) : (
         <LoadingSpinner />
